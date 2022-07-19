763ffb1d65a0a7bba9d327509327dafd1ecde800
Rafael Vigo <rvigo07+github@gmail.com>
2022-07-18 22:02:19 -0300

    ci: includes ci workflow files

---
+++ .github/utils/dummy.go
@@ -0,0 +1,6 @@
+package main
+
+func main() {
+	// this is a workaround to release rust binaries with goreleaser
+	// ref: https://jondot.medium.com/shipping-rust-binaries-with-goreleaser-d5aa42a46be0s
+}
