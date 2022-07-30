108c6ba40cbb47b9b191875f87c8743656ceee20
Rafael Vigo <rvigo07+github@gmail.com>
2022-07-30 01:32:16 -0300

    fix: set the cursor in the correct line based on widget width

--- src/gui/layouts/cursor.rs
+++ src/gui/layouts/cursor.rs
@@ -1,8 +1,10 @@
+use crate::gui::entities::field::Field;
 use tui::{backend::Backend, layout::Rect, Frame};
 use unicode_width::UnicodeWidthStr;
 
-use crate::gui::entities::field::Field;
-
 pub fn set_cursor_positition<B: Backend>(frame: &mut Frame<B>, item: &Field, area: Rect) {
-    frame.set_cursor(area.x + item.input.width() as u16 + 1, area.y + 1);
+    let y: u16 = area.y + 1 + (item.input.width() as u16 / (area.width - 2));
+    let x: u16 = area.x + 1 + (item.input.width() as u16 % (area.width - 2));
+
+    frame.set_cursor(x, y);
 }
