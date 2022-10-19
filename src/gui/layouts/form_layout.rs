13ceb6b4a29f1c2e20bc0906764ced83c79ddade
Rafael Vigo <rvigo07+github@gmail.com>
2022-10-19 01:19:03 +0000

    style: improves input and edit layout

--- src/gui/layouts/form_layout.rs
+++ src/gui/layouts/form_layout.rs
@@ -1,6 +1,6 @@
 use super::{
     help_layout::{render_help, render_helper_footer},
-    layout_utils::render_widget,
+    layout_utils::{get_main_block, render_widget},
     popup_layout::render_popup,
 };
 use crate::gui::entities::{field::FieldType, state::State};
@@ -18,16 +18,26 @@ pub fn render<B: Backend>(frame: &mut Frame<B>, state: &mut State) {
         .margin(1)
         .constraints(
             [
-                Constraint::Length(5),
-                Constraint::Min(10),
-                Constraint::Length(5),
-                Constraint::Length(3),
+                Constraint::Min(10),   //Form
+                Constraint::Length(3), //Help
             ]
             .as_ref(),
         )
         .split(frame.size());
+    let form_chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .margin(1)
+        .constraints(
+            [
+                Constraint::Length(5), //Alias & Namespace
+                Constraint::Min(10),   //Command
+                Constraint::Length(5), //Desc & Tags
+            ]
+            .as_ref(),
+        )
+        .split(chunks[0]);
 
-    let block = Block::default()
+    let form_block = Block::default()
         .borders(Borders::ALL)
         .style(Style::default())
         .title(format!(" {} ", state.view_mode))
@@ -35,21 +45,22 @@ pub fn render<B: Backend>(frame: &mut Frame<B>, state: &mut State) {
     let first_row = Layout::default()
         .direction(Direction::Horizontal)
         .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
-        .split(chunks[0]);
+        .split(form_chunks[0]);
     let second_row = Layout::default()
         .direction(Direction::Horizontal)
         .constraints([Constraint::Percentage(100)].as_ref())
-        .split(chunks[1]);
+        .split(form_chunks[1]);
     let third_row = Layout::default()
         .direction(Direction::Horizontal)
         .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
-        .split(chunks[2]);
+        .split(form_chunks[2]);
     let fourth_row = Layout::default()
         .direction(Direction::Vertical)
         .constraints([Constraint::Length(3)].as_ref())
-        .split(chunks[3]);
+        .split(chunks[1]);
 
-    frame.render_widget(block, frame.size());
+    frame.render_widget(get_main_block(), frame.size());
+    frame.render_widget(form_block, chunks[0]);
 
     for field in state.field_context.fields().iter() {
         match field.field_type() {
