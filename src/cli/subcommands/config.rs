b6074b5b0e3c29940d50a352741888fa33418ad6
Rafael Vigo <rvigo07+github@gmail.com>
2023-03-15 19:46:48 -0300

    chore: improves widget installation steps

--- src/cli/subcommands/config.rs
+++ src/cli/subcommands/config.rs
@@ -65,18 +65,19 @@ pub struct Widget {
 pub fn config_subcommand(config: Config, mut app_config: AppConfig) -> Result<()> {
     if let Some(ConfigSubcommand::ZshWidget(_)) = config.subcommand {
         install_zsh_widget(app_config.get_app_home_dir())?
-    }
-    if let Some(quiet) = config.default_quiet_mode {
-        app_config.set_default_quiet_mode(quiet)?;
-        println!("quiet mode set to {quiet}")
-    }
-    if let Some(log_level) = config.default_log_level {
-        app_config.set_log_level(log_level.as_config_enum())?;
-        println!("log level set to {log_level:?}")
-    }
-    if let Some(highlight) = config.highlight_matches {
-        app_config.set_highlight(highlight)?;
-        println!("highlight matches set to {highlight}")
+    } else {
+        if let Some(quiet) = config.default_quiet_mode {
+            app_config.set_default_quiet_mode(quiet)?;
+            println!("quiet mode set to {quiet}")
+        }
+        if let Some(log_level) = config.default_log_level {
+            app_config.set_log_level(log_level.as_config_enum())?;
+            println!("log level set to {log_level:?}")
+        }
+        if let Some(highlight) = config.highlight_matches {
+            app_config.set_highlight(highlight)?;
+            println!("highlight matches set to {highlight}")
+        }
     }
 
     Ok(())
@@ -89,7 +90,7 @@ fn install_zsh_widget(app_home_dir: PathBuf) -> Result<()> {
         }
     }
 
-    validate_fzf();
+    validate_fzf()?;
 
     let widget = include_str!("../resources/zsh/cl-exec-widget");
     let dest_location = app_home_dir.join("cl-exec-widget");
@@ -104,15 +105,13 @@ fn install_zsh_widget(app_home_dir: PathBuf) -> Result<()> {
     Ok(())
 }
 
-fn validate_fzf() {
-    if let Ok(res) = Command::new("zsh")
-        .arg("-c")
-        .arg("$(command -v fzf)")
-        .output()
-    {
-        if !res.status.success() {
-            eprintln!("Please first install fzf and then install de widget")
-        }
+fn validate_fzf() -> Result<()> {
+    let output = Command::new("command").args(["-v", "fzf"]).output()?;
+
+    if !output.status.success() {
+        bail!("This widget needs fzf to work. Please first install it and then reinstall de widget")
+    } else {
+        Ok(())
     }
 }
 
