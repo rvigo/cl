5f8748d15aa005a78eac78039e126eeb49ec2300
Rafael Vigo <rvigo07+github@gmail.com>
2023-03-15 20:30:20 -0300

    chore(lint): removes borrowing

--- src/resources/config.rs
+++ src/resources/config.rs
@@ -153,7 +153,7 @@ impl Config {
         }
         let config_file_path = app_home_dir.join(self.get_config_file_path()?);
         let config_data = toml::to_string(self)?;
-        write(&config_file_path, config_data)?;
+        write(config_file_path, config_data)?;
         Ok(())
     }
 
