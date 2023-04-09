6442102725644e069fac5bf907a8097f618ed1e6
Rafael Vigo <rvigo07+github@gmail.com>
2023-04-09 17:54:49 -0300

    feat: improves zsh widget output

--- src/cli/subcommands/misc.rs
+++ src/cli/subcommands/misc.rs
@@ -6,6 +6,7 @@ use crate::{
 use anyhow::Result;
 use clap::Parser;
 use owo_colors::{colors::CustomColor, OwoColorize};
+use std::collections::HashSet;
 
 #[derive(Parser)]
 pub struct Misc {
@@ -24,71 +25,97 @@ pub fn misc_subcommand(misc: Misc, config: Config) -> Result<()> {
         FileService::new(config.get_command_file_path()?).load_commands_from_file()?;
     let commands = Commands::init(command_list);
     if misc.description {
-        if let Some(alias) = misc.alias {
-            let command = commands.find_command(alias, misc.namespace)?;
-            print_colorized_command(command);
+        if let Some(alias) = &misc.alias {
+            let namespace = misc.namespace;
+            let command = commands.find_command(alias.clone(), namespace)?;
+            println!("{}", command.to_color_string());
         }
     } else if misc.fzf {
-        commands
+        let mut seen = HashSet::with_capacity(commands.command_list().len());
+        let duplicated: Vec<String> = commands
             .command_list()
             .iter()
-            .for_each(|c| println!("{}", c.alias))
+            .filter_map(|c| {
+                if seen.contains(&c.alias) {
+                    Some(c.alias.to_owned())
+                } else {
+                    seen.insert(c.alias.to_owned());
+                    None
+                }
+            })
+            .collect();
+
+        commands.command_list().iter().for_each(|c| {
+            if duplicated.contains(&c.alias) {
+                println!(
+                    "{} ({})",
+                    c.alias,
+                    c.namespace.fg::<CustomColor<201, 165, 249>>()
+                )
+            } else {
+                println!("{}", c.alias)
+            }
+        })
     } else {
         commands
             .command_list()
             .iter()
-            .for_each(|c| println!("{}", command_to_string(c.to_owned())));
+            .for_each(|c| println!("{}", c.sumarize()));
     }
 
     Ok(())
 }
 
-fn command_to_string(command: Command) -> String {
-    if let Some(desc) = command.description {
+trait ToColorString {
+    /// Returns a colored String representation of `self`
+    fn to_color_string(&self) -> String;
+}
+
+impl ToColorString for Command {
+    fn to_color_string(&self) -> String {
         format!(
-            "{}.{}: {} --> {}",
-            command.namespace,
-            command.alias,
-            desc,
-            sanitize_string(command.command)
-        )
-    } else {
-        format!(
-            "{}.{} --> {}",
-            command.namespace,
-            command.alias,
-            sanitize_string(command.command)
+            "Alias: {}\nNamespace: {}\nDescription: {}\nTags: {}\nCommand: {}",
+            self.alias.fg::<CustomColor<201, 165, 249>>(),
+            self.namespace.fg::<CustomColor<201, 165, 249>>(),
+            self.description
+                .as_ref()
+                .unwrap_or(&String::default())
+                .fg::<CustomColor<201, 165, 249>>(),
+            self.tags_as_string().fg::<CustomColor<201, 165, 249>>(),
+            self.command.fg::<CustomColor<201, 165, 249>>(),
         )
     }
 }
 
-fn sanitize_string(command: String) -> String {
-    let max_lenght_command: String = command.chars().take(50).collect();
-    if max_lenght_command.contains('\n') {
-        let idx = command.find('\n').unwrap_or(51);
-        let short_command = format!("{}{}", &command[..(idx)], "...");
-        short_command
-    } else if max_lenght_command.len() == 50 {
-        let short_command = format!("{}{}", &command[..50], "...");
-        short_command
-    } else {
-        command
-    }
+trait Sumarize {
+    /// Returns a sumarized string of `self`
+    fn sumarize(&self) -> String;
 }
 
-fn print_colorized_command(command: Command) {
-    println!(
-        "Alias: {}\nNamespace: {}\nDescription: {}\nTags: {}\nCommand: {}",
-        command.alias.fg::<CustomColor<201, 165, 249>>(),
-        command.namespace.fg::<CustomColor<201, 165, 249>>(),
-        command
-            .description
-            .as_ref()
-            .unwrap_or(&String::default())
-            .fg::<CustomColor<201, 165, 249>>(),
-        command.tags_as_string().fg::<CustomColor<201, 165, 249>>(),
-        command.command.fg::<CustomColor<201, 165, 249>>(),
-    )
+impl Sumarize for Command {
+    fn sumarize(&self) -> String {
+        let command_string = &self.command;
+        let max_lenght_command: String = command_string.chars().take(50).collect();
+        let command_string = if max_lenght_command.contains('\n') {
+            let idx = self.command.find('\n').unwrap_or(51);
+            let short_command = format!("{}{}", &self.command[..(idx)], "...");
+            short_command
+        } else if max_lenght_command.len() == 50 {
+            let short_command = format!("{}{}", &self.command[..50], "...");
+            short_command
+        } else {
+            self.command.to_owned()
+        };
+
+        if let Some(ref desc) = self.description {
+            format!(
+                "{}.{}: {} --> {}",
+                self.namespace, self.alias, desc, command_string
+            )
+        } else {
+            format!("{}.{} --> {}", self.namespace, self.alias, command_string)
+        }
+    }
 }
 
 #[cfg(test)]
@@ -98,42 +125,26 @@ mod test {
     #[test]
     fn should_format_a_command_with_description_to_string() {
         let command = Command {
-            namespace: "namespace".to_string(),
-            command: "command".to_string(),
-            description: Some("description".to_string()),
-            alias: "alias".to_string(),
+            namespace: "namespace".to_owned(),
+            command: "command".to_owned(),
+            description: Some("description".to_owned()),
+            alias: "alias".to_owned(),
             tags: None,
         };
-        let expected_output = "namespace.alias: description --> command".to_string();
-        assert_eq!(command_to_string(command), expected_output);
+        let expected_output = "namespace.alias: description --> command".to_owned();
+        assert_eq!(command.sumarize(), expected_output);
     }
 
     #[test]
     fn should_format_a_command_without_description_to_string() {
         let command = Command {
-            namespace: "namespace".to_string(),
-            command: "command".to_string(),
+            namespace: "namespace".to_owned(),
+            command: "command".to_owned(),
             description: None,
-            alias: "alias".to_string(),
+            alias: "alias".to_owned(),
             tags: None,
         };
-        let expected_output = "namespace.alias --> command".to_string();
-        assert_eq!(command_to_string(command), expected_output);
-    }
-
-    #[test]
-    fn should_sanitize_a_long_command_string() {
-        let long_command = "a".repeat(60);
-        let short_command = "a".repeat(40);
-
-        assert_eq!(sanitize_string(long_command), "a".repeat(50) + "...");
-        assert_eq!(sanitize_string(short_command.clone()), short_command);
-    }
-
-    #[test]
-    fn should_sanitize_a_command_string_with_newline() {
-        let input = "multiline command\n".to_string();
-        let expected_output = "multiline command...".to_string();
-        assert_eq!(sanitize_string(input), expected_output);
+        let expected_output = "namespace.alias --> command".to_owned();
+        assert_eq!(command.sumarize(), expected_output);
     }
 }
